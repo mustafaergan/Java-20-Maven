a9f6a7afe5d0b9520157c684dbdfc4451dec66f7
vektorel <vektorel@lab4-ogr>
2017-06-11 12:44:38 +0300

    maven giris

---
+++ Marka/src/com/mustafaergan/marka/Fabrika.java
@@ -0,0 +1,17 @@
+package com.mustafaergan.marka;
+
+public class Fabrika {
+	
+	private void markaUret() {
+		System.out.println("marka uretiyorum");
+	}
+	
+	/**
+	 * Maven Amaclari
+	 * Clean = taraget içersinde projeyi siler
+	 * Compile = Kodlarýmý derler ve atar
+	 * Package = pom belirtmiþ oldugum paketleme sistemine cevirir
+	 * 
+	 */
+
+}
