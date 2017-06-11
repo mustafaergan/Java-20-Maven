d64c3628d936985f077ba3e9b02894e24150b85d
vektorel <vektorel@lab4-ogr>
2017-06-11 13:06:24 +0300

    clean compile install package

--- Marka/src/com/mustafaergan/marka/Fabrika.java
+++ Marka/src/com/mustafaergan/marka/Fabrika.java
@@ -1,9 +1,13 @@
 package com.mustafaergan.marka;
 
+import com.google.gson.Gson;
+
 public class Fabrika {
 	
 	private void markaUret() {
 		System.out.println("marka uretiyorum");
+		
+		Gson gson = new Gson();
 	}
 	
 	/**
@@ -11,7 +15,7 @@ public class Fabrika {
 	 * Clean = taraget içersinde projeyi siler
 	 * Compile = Kodlarýmý derler ve atar
 	 * Package = pom belirtmiþ oldugum paketleme sistemine cevirir
-	 * 
+	 * install = benim kodumu .m2 dosyasina atar
 	 */
 
 }
